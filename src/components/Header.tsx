0422b98e08725cf8a66d1127c394984a6f292b89
felixleduc <felixantoineleduc@gmail.com>
2025-09-25 08:27:20 -0400

    AJout de l'icone de compte

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { User } from "@/types/user";
+import { UserIcon } from "lucide-react";
 
 export default function Header() {
   const [user, setUser] = useState<User | null>(null);
@@ -48,18 +49,19 @@ export default function Header() {
           height={247}
           className="w-[128px] h-auto"
         />
-        <div>
-          {user ? (
+        {user ? (
+          <div className="flex items-center gap-4">
+            <UserIcon color="black" size={24} fill="black" />
             <p>{user.name}</p>
-          ) : (
-            <a
-              href="#"
-              className="border-2 border-black rounded-4xl px-4 py-2 hover:bg-[#02dcde] transition-all duration-300"
-            >
-              Connexion
-            </a>
-          )}
-        </div>
+          </div>
+        ) : (
+          <a
+            href="#"
+            className="border-2 border-black rounded-4xl px-4 py-2 hover:bg-[#02dcde] transition-all duration-300"
+          >
+            Connexion
+          </a>
+        )}
       </header>
     </div>
   );
