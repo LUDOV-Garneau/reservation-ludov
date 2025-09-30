ff5c50aaca8a4992620bc87b9edeb404e7994ca5
Fabio345i <famor002@edu.cegepgarneau.ca>
2025-09-30 16:49:16 -0400

    fix: changement du POST de la console

--- src/app/api/consoles/select-console/route.ts
+++ src/app/api/consoles/select-console/route.ts
@@ -1,29 +1,6 @@
-import { NextRequest, NextResponse } from "next/server";
-import pool from "@/lib/db";
+import { NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) {
-  try {
-    const { consoleId } = await request.json();
-    if (!consoleId) {
-      return NextResponse.json(
-        { message: "ID de console manquant" },
-        { status: 400 }
-      );
-    }
-    const [result] = await pool.query(
-      "UPDATE consoles SET available = 0 WHERE id = ? AND available = 1",
-      [consoleId]
-    );
-    const updateResult = result as { affectedRows: number };
-    if (updateResult.affectedRows === 0) {
-      return NextResponse.json(
-        { message: "Console non disponible" },
-        { status: 400 }
-      );
-    }
-    return NextResponse.json({ message: "Console sélectionnée avec succès" });
-  } catch (err) {
-    console.error(err);
-    return NextResponse.json({ message: "Erreur serveur" }, { status: 500 });
-  }
+    // TODO
+    // Implémenter la logique pour gérer la sélection d'une console
 }
