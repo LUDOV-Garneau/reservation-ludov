3271677244e33eb5d0285dd6ef0051f37b51f9aa
Andres Solano <josol002@edu.cegepgarneau.ca>
2025-11-13 10:55:22 -0500

    Appel au component ReservationTable

--- src/components/admin/ReservationsTab.tsx
+++ src/components/admin/ReservationsTab.tsx
@@ -1,28 +1,12 @@
 "use client";
 
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import ReservationTable from "@/components/admin/reservations/ReservationTable";
 import { TabsContent } from "@/components/ui/tabs";
-import { useTranslations } from "next-intl";
-import ReservationsTable from "@/components/admin/components/ReservationTable"
 
 export default function ReservationsTab() {
-  const t = useTranslations();
-
   return (
     <TabsContent value="reservations">
-      <Card>
-        <CardHeader>
-          <CardTitle>{t("admin.reservations.title")}</CardTitle>
-          <CardDescription>
-            <ReservationsTable refreshKey={0} />
-          </CardDescription>
-        </CardHeader>
-      </Card>
+      <ReservationTable />
     </TabsContent>
   );
 }
