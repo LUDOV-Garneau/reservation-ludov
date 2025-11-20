4a0d6e46dbe3480480269dc37e3c714d3adb175a
felixleduc <felixantoineleduc@gmail.com>
2025-11-20 09:53:14 -0500

    Feat: ajout d'une banniere de cookies et de la page de confidentialite

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,10 +2,12 @@ import { hasLocale, NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import { Toaster } from "@/components/ui/sonner";
+import { CookieBanner } from "@/components/layout/CookieBanner";
 
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }));
 }
+
 
 export default async function RootLayout({
   children,
@@ -23,6 +25,7 @@ export default async function RootLayout({
   return (
     <NextIntlClientProvider locale={locale} messages={messages}>
       {children}
+      <CookieBanner />
       <Toaster position="top-center" richColors closeButton />
     </NextIntlClientProvider>
   );
