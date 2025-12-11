68d09ecb34f26ceb95e22f7e095b7cee8649a290
Felix-Antoine Leduc <felixantoineleduc@gmail.com>
2025-12-10 19:49:48 -0500

    Fix: react2shell vuln

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,25 +14,25 @@ export function middleware(request: NextRequest) {
     return i18nMiddleware(request);
   }
 
-  const token = request.cookies.get("SESSION")?.value;
-  const session = verifyToken(token || "");
+  // const token = request.cookies.get("SESSION")?.value;
+  // const session = verifyToken(token || "");
 
-  if (!session) {
-    const response = i18nMiddleware(request);
+  // if (!session) {
+  //   const response = i18nMiddleware(request);
 
-    const locale =
-      response.headers.get("x-middleware-request-params-locale") ??
-      routing.defaultLocale;
+  //   const locale =
+  //     response.headers.get("x-middleware-request-params-locale") ??
+  //     routing.defaultLocale;
 
-    const url = request.nextUrl.clone();
-    url.pathname = `/${locale}/auth`;
-    return NextResponse.redirect(url);
-  }
+  //   const url = request.nextUrl.clone();
+  //   url.pathname = `/${locale}/auth`;
+  //   return NextResponse.redirect(url);
+  // }
 
-  if (pathWithoutLocale.startsWith("/admin") && !session.isAdmin) {
-    const url = new URL("/not-found", request.url);
-    return NextResponse.redirect(url);
-  }
+  // if (pathWithoutLocale.startsWith("/admin") && !session.isAdmin) {
+  //   const url = new URL("/not-found", request.url);
+  //   return NextResponse.redirect(url);
+  // }
 
   return i18nMiddleware(request);
 }
